b2f9357fa8a61ca844419013371465c7978d9390
Rick Helmus <r.helmus@uva.nl>
2023-08-05 21:17:17 +0200

    exclusion env variables

--- tools/download_ext.R
+++ tools/download_ext.R
@@ -5,28 +5,34 @@ downloads <- list(
         url = sprintf("https://github.com/boecker-lab/sirius/releases/download/v5.6.3/sirius-5.6.3-%s64-headless.zip",
                       switch(Sys.info()[["sysname"]], Windows = "win", Linux = "linux", Darwin = "osx")),
         dest = "sirius.zip",
-        destUnZip = "."
+        destUnZip = ".",
+        exclude = "SIRIUS"
     ),
     MetFrag = list(
         url = "https://github.com/ipb-halle/MetFragRelaunched/releases/download/v.2.5.0/MetFragCommandLine-2.5.0.jar",
-        dest = "MetFragCommandLine.jar"
+        dest = "MetFragCommandLine.jar",
+        exclude = "METFRAGCL"
     ),
     CompTox = list(
         url = "https://zenodo.org/record/3472781/files/CompTox_07March19_WWMetaData.csv",
-        dest = "CompToxWW.csv"
+        dest = "CompToxWW.csv",
+        exclude = "METFRAGCT"
     ),
     PubChemLite = list(
         url = "https://zenodo.org/record/8191746/files/PubChemLite_exposomics_20230728.csv",
-        dest = "PubChemLite.csv"
+        dest = "PubChemLite.csv",
+        exclude = "METFRAGPCL"
     ),
     BioTransformerFiles = list(
         url = "https://bitbucket.org/djoumbou/biotransformer/get/master.zip",
         dest = "biotransformer_files.zip",
-        destUnZip = "."
+        destUnZip = ".",
+        exclude = "BIOTRANSFORMER"
     ),
     BioTransformerJar = list(
         url = "https://github.com/rickhelmus/patRoonDeps/raw/master/ext/biotransformer-3.0.0.jar",
-        dest = "biotransformer-3.0.0.jar"
+        dest = "biotransformer-3.0.0.jar",
+        exclude = "BIOTRANSFORMER"
     )
 )
 
@@ -35,6 +41,9 @@ dir.create(destPath, recursive = TRUE, showWarnings = FALSE)
 
 for (ext in names(downloads))
 {
+    if (nzchar(Sys.getenv(paste0("PATROONEXT_NO_", downloads[[ext]]$exclude))))
+        next
+    
     p <- file.path(destPath, downloads[[ext]]$dest)
     if (downloadFile(ext, downloads[[ext]]$url, p))
     {
@@ -63,6 +72,8 @@ if (length(BTDir) == 1)
 # Install OpenBabel/OpenMS on Windows
 if (Sys.info()[["sysname"]] == "Windows")
 {
-    unzipFile("openbabel.zip", file.path(destPath, "openbabel"))
-    unzipFile("openms.zip", file.path(destPath, "openms"))
+    if (!nzchar(Sys.getenv("PATROONEXT_NO_OPENBABEL")))
+        unzipFile("openbabel.zip", file.path(destPath, "openbabel"))
+    if (!nzchar(Sys.getenv("PATROONEXT_NO_OPENMS")))
+        unzipFile("openms.zip", file.path(destPath, "openms"))
 }
